1df6827cc1fc152a645ff22b62941c2aafee0bfa
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-13 20:42:02 +0400

    Create tables 'users' for db (migration)
    Наполняем кодом, описывающим создание таблицы 'Users' в базе данных. С полями и типами.

--- db/migrate/20220613161032_create_users.rb
+++ db/migrate/20220613161032_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+  	create_table :users do |t|
+ 		t.text :name
+ 		t.text :login
+ 		t.text :pass_main
+ 		t.text :pass_guest
+
+ 		t.timestamps 
+  	end	
   end
 end
