e7e2f549ba6a4e6dac43c841eb39b8db9bdc67d3
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-13 19:51:09 +0400

    Del initialize

--- app.rb
+++ app.rb
@@ -5,21 +5,21 @@ require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
 require_relative './helpers/for_db.rb' # Методы для работы с БАЗОЙ ДАННЫХ
-require_relative './helpers/initialize.rb' 
 require_relative './helpers/persons.rb'
 require_relative './helpers/users.rb' 
 
-get '/' do
-	db = get_db
-	@persons = db.execute "SELECT * FROM Persons WHERE id_user=? ", [$user[0]['id']]
+set :database, "sqlite3:remebe_HQ.db"
 
-	erb :index
+class Users < ActiveRecord::Base 
+end
+
+class Person < ActiveRecord::Base 
+end 
+
+get '/' do
+	erb "Hole"
 end
 
 get '/about' do
 	erb :about
 end	
-
-$debug_user = 'Opr' # Отладочный пользователь
-db = get_db
-$user = db.execute "SELECT * FROM Users WHERE login=? ", [$debug_user]
