3d687fb74991293f3b1e594b2cf16c5d5fb20688
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-07-22 23:43:19 +0400

    Add new Person

--- db/migrate/20220613181746_create_persons.rb
+++ db/migrate/20220613181746_create_persons.rb
@@ -1,16 +1,16 @@
 class CreatePersons < ActiveRecord::Migration[5.2]
   def change
-  	create_table :people do |t|
-  		t.text	:name
-  		t.text  :b_date
-  		t.text  :d_date
-  		t.text  :photo_link
-  		t.text  :discription
+    create_table :people do |t|
+      t.text  :name
+      t.text  :b_date
+      t.text  :d_date
+      t.text  :photo_link
+      t.text  :discription
 
-  		t.timestamps
-  	end 
+      t.timestamps
+    end 
 
-  	
+    # id = 1
     Person.create ({
       :name => 'ОПРЕДЕЛЕНЦЕВ Виктор Степанович',
       :b_date => '11.02.1936', 
@@ -19,6 +19,7 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :discription => 'Мой отец.'
     }) 
 
+    # id = 2
     Person.create ({
       :name => 'ОПРЕДЕЛЕНЦЕВ Степан Алексеевич',
       :b_date => '__.__.19__', 
@@ -27,6 +28,7 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :discription => 'Мой дед по отцовской линии.'
     }) 
 
+    # id =  3
     Person.create ({
       :name => 'ОПРЕДЕЛЕНЦЕВА Аграфена Васильевна',
       :b_date => '28.05.1900', 
@@ -34,6 +36,17 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :photo_link => '0w_.jpg', 
       :discription => 'Моя бабушка по отцовской линии.'
     }) 
+
+    # id = 4
+    Person.create ({
+      :name => 'ОПРЕДЕЛЕНЦЕВ Александр Степанович',
+      :b_date => '__.__.1924', 
+      :d_date => '19.07.1944', 
+      :photo_link => '0m_.jpg', 
+      :discription => 'Мой дядя по отцовской линии. Старший брат моего отца'
+    }) 
+
+    # id = 5
     Person.create ({
       :name => 'ПАНКОВ Николай Петрович',
       :b_date => '22.05.1915', 
@@ -42,6 +55,7 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :discription => 'Мой дед по материнской линии.'
     }) 
 
+    # id = 6
     Person.create ({
       :name => 'ПАНКОВА Мария Тимофеевна',
       :b_date => '14.06.1914', 
@@ -50,14 +64,16 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :discription => 'Моя бабушка по материнской линии.'
     }) 
 
+    # id = 7
     Person.create ({
       :name => 'НИКИПЕЛОВА Галина Николаевна',
       :b_date => '02.09.1939', 
       :d_date => '11.12.2021', 
-      :photo_link => '6_.jpg', 
+      :photo_link => '7_.jpg', 
       :discription => 'Моя тётя, сестра моей мамы'
     }) 
 
+    # id = 8
     Person.create ({
       :name => 'ПЛАТОНОВ Валерий Николаевич',
       :b_date => '__.__.19__', 
@@ -66,13 +82,14 @@ class CreatePersons < ActiveRecord::Migration[5.2]
       :discription => 'Мой двоюродный брат.'
     }) 
 
-  	Person.create ({
-  		:name => 'БАРАБАНЩИКОВ Александр Викторович', 
-  		:b_date => 'нет данных', 
-  		:d_date => '__.06.20__', 
-  		:photo_link => '0m_.jpg', 
-  		:discription => 'Мы вместе работали в БЭС. Мой второй учитель по компьютеру.'
-  	}) 	
+    # id = 9
+    Person.create ({
+      :name => 'БАРАБАНЩИКОВ Александр Викторович', 
+      :b_date => 'нет данных', 
+      :d_date => '__.06.20__', 
+      :photo_link => '0m_.jpg', 
+      :discription => 'Мы вместе работали в БЭС. Мой второй учитель по компьютеру.'
+    })  
 
   end
 end
